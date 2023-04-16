fbdae0adf1abfe2fde2fd9f4d5c59fadf14df857
shyedhu <shyedhu@users.noreply.github.com>
2023-04-15 22:46:25 -0700

    updated

--- page.test.js
+++ page.test.js
@@ -1,30 +1,6 @@
-import { Selector } from 'testcafe';
+fixture`Validate the nike.com page`
+  .page`https://www.nike.com/w`;
 
-
-// fixture`Validate the nike.com checkout`
-//   .page`https://www.nike.com/t/headbands-6-pack-mh3PSb/N0002545-176`;
-
-// test('Add to bag and navigate to Checkout page', async (t) => {
-//   const atc = Selector('[data-browse-component="ATCButton"] .add-to-cart-btn');
-//   const carticon = Selector('#nav-cart > a > div > span');
-
-//   // add to bag
-//   await t.click(atc);
-//   await t.debug();
-  
-  
-// });
-
-fixture`Validate the nike.com checkout`
-  .page`https://www.nike.com/t/gift-card-RpyEVO`;
-
-test('Add to bag and navigate to Checkout page', async (t) => {
-  const atc = Selector('[data-browse-component="ATCButton"] .add-to-cart-btn');
-  const carticon = Selector('#nav-cart > a > div > span');
-
-  // add to bag
-  await t.click(atc);
+test('verify nike.com page', async (t) => {
   await t.debug();
-  
-  
 });
