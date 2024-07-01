bfce742bc9cadc0609a6cde5fc5ba695855f71e3
shyedhu <shyedhu@users.noreply.github.com>
2024-07-01 16:52:19 -0700

    Update page.test.js

--- page.test.js
+++ page.test.js
@@ -1,6 +1,10 @@
-fixture`Validate the nike.com page`
-  .page`https://www.nike.com/w`;
+fixture`Validate input on the page`
+  .page`https://www.nike.com/cart`;
 
-test('verify nike.com page', async (t) => {
-  await t.debug();
+test('verify the page', async (t) => {
+  await t.wait(5000);
+  await t.click('#promo-codes > summary > span > span > svg')
+  await t.wait(5000);
+  await t.typeText('#promo-codes > div > span > form > div.css-19ka2tk.e1mkeel42 > div > input','ABC')
+  await t.wait(8000);
 });
