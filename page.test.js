bacec98dbd6381bda8953e133359b2e08abd2cad
shyedhu <shyedhu@users.noreply.github.com>
2024-10-10 18:08:33 -0700

    Update page.test.js

--- page.test.js
+++ page.test.js
@@ -1,10 +1,6 @@
 fixture`Validate input on the page`
-  .page`https://www.nike.com/cart`;
+  .page`https://the-internet.herokuapp.com/basic_auth`;
 
 test('verify the page', async (t) => {
   await t.wait(5000);
-  await t.click('#promo-codes > summary > span > span > svg')
-  await t.wait(5000);
-  await t.typeText('#promo-codes > div > span > form > div.css-19ka2tk.e1mkeel42 > div > input','ABC')
-  await t.wait(8000);
 });
